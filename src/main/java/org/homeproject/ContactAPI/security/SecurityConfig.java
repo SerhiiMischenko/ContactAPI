c078235cfc30795bcea35882aa08815f9e204452
SerhiiMischenko <serj.roshen@gmail.com>
2023-06-28 19:17:08 +0300

    Rework SecurityConfig.java

--- src/main/java/org/homeproject/ContactAPI/security/SecurityConfig.java
+++ src/main/java/org/homeproject/ContactAPI/security/SecurityConfig.java
@@ -7,9 +7,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import javax.sql.DataSource;
 
 @Configuration
@@ -26,10 +26,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/contact/**").hasAnyRole("ADMIN", "USER")
                 .and()
-                .formLogin()
+                .httpBasic()
                 .and()
-                .logout()
-                .logoutSuccessUrl("/")
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .csrf().disable();
     }
