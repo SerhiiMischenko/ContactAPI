19a5ba4d789d4463c2d3356e5963a5fe181181ee
SerhiiMischenko <serj.roshen@gmail.com>
2023-07-13 12:46:34 +0300

    Fix some bugs in UserController.java in method GET

--- src/main/java/org/homeproject/ContactAPI/config/security/SecurityConfig.java
+++ src/main/java/org/homeproject/ContactAPI/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.homeproject.ContactAPI.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,7 +24,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/user/create").anonymous()
+                .antMatchers( HttpMethod.POST,"/user").anonymous()
+                .antMatchers(HttpMethod.GET,"/user").hasRole("ADMIN")
                 .and()
                 .httpBasic()
                 .and()
--- src/main/java/org/homeproject/ContactAPI/controller/UserController.java
+++ src/main/java/org/homeproject/ContactAPI/controller/UserController.java
@@ -14,7 +14,6 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
