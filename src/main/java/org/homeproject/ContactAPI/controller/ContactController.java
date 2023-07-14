399e3c8d3d8fd16086c88c7ba224936a2fc9c5ee
SerhiiMischenko <serj.roshen@gmail.com>
2023-07-14 13:13:33 +0300

    Rework ContactController.java. Controller only call ContactService. All logic relocate to ContactService

--- src/main/java/org/homeproject/ContactAPI/controller/ContactController.java
+++ src/main/java/org/homeproject/ContactAPI/controller/ContactController.java
@@ -2,24 +2,15 @@ package org.homeproject.ContactAPI.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.homeproject.ContactAPI.dto.ContactDTO;
 import org.homeproject.ContactAPI.entity.Contact;
 import org.homeproject.ContactAPI.error.ErrorResponse;
-import org.homeproject.ContactAPI.error.InvalidPhoneNumberException;
 import org.homeproject.ContactAPI.service.ContactService;
 import org.homeproject.ContactAPI.service.UserService;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.homeproject.ContactAPI.entity.User;
 
 @RestController
 @RequestMapping("/contact")
@@ -41,89 +32,21 @@ public class ContactController {
     @Secured("ROLE_USER")
     @PostMapping()
     public ResponseEntity<?> createContact(@RequestBody Contact contact, Authentication authentication) {
-        if (authentication == null || authentication.getName() == null) {
-            errorResponse.statusNotAuthorized("You are no authorized", "/contact");
-            return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
-        }
-        Contact newContact =new Contact();
-        try {
-            String currentUserName = authentication.getName();
-            User currentUser = userService.getUserByUsername(currentUserName);
-            if (authentication.isAuthenticated() && currentUser.getRole().equals("ROLE_USER")) {
-                newContact =
-                        new Contact(currentUser, contact.getFirstName(), contact.getLastName(), contact.getPhoneNumber());
-                return new ResponseEntity<>(contactService.createContact(newContact), HttpStatus.OK);
-            } else {
-                errorResponse.statusNotAuthorized("You are no authorized", "/contact");
-                return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
-            }
-
-        } catch (InvalidPhoneNumberException e) {
-                errorResponse.statusNotValid("Not valid phone number", "/contact");
-                return new ResponseEntity<>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
-        } catch (Exception e) {
-            return new ResponseEntity<>(contactService.createContact(newContact), HttpStatus.OK);
-        }
+        return contactService.createContact(contact, authentication);
     }
 
     @ApiOperation(value = "Get all user contacts", notes = "Returns contacts list")
     @Secured({"ROLE_ADMIN", "ROLE_USER"})
     @GetMapping()
     public ResponseEntity<?> getContacts(Authentication authentication) {
-        if (authentication == null || authentication.getName() == null) {
-            errorResponse.statusNotAuthorized("You are no authorized", "/contact");
-            return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
-        }
-        String currentUsername = authentication.getName();
-        User currentUser = userService.getUserByUsername(currentUsername);
-        if (authentication.isAuthenticated() && currentUser.getRole().equals("ROLE_ADMIN")) {
-            ModelMapper modelMapper = new ModelMapper();
-            List<Contact> allContacts = contactService.readAllContacts();
-            List<ContactDTO> allContactsDTO = allContacts.stream()
-                    .map(contact -> modelMapper.map(contact, ContactDTO.class))
-                    .collect(Collectors.toList());
-            return new ResponseEntity<>(allContactsDTO, HttpStatus.OK);
-        } else {
-            List<Contact> userContacts = contactService.getContactListByUserId(currentUser.getId());
-
-            ModelMapper modelMapper = new ModelMapper();
-            List<ContactDTO> contactDTOList = userContacts.stream()
-                    .map(contact -> modelMapper.map(contact, ContactDTO.class))
-                    .collect(Collectors.toList());
-
-            return new ResponseEntity<>(contactDTOList, HttpStatus.OK);
-        }
+        return contactService.readAllContacts(authentication);
     }
 
     @ApiOperation(value = "Get contact by ID", notes = "Returns a single contact based on ID")
     @Secured({"ROLE_ADMIN", "ROLE_USER"})
     @GetMapping("/{id}")
     public ResponseEntity<?> getContactByID(@PathVariable("id") Long id, Authentication authentication) {
-        if (authentication == null || authentication.getName() == null) {
-            errorResponse.statusNotAuthorized("You are no authorized", "/contact");
-            return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
-        }
-        String currentName = authentication.getName();
-        User currentUser = userService.getUserByUsername(currentName);
-        try {
-            Contact findContact = contactService.readContactById(id);
-            if (authentication.isAuthenticated() && findContact.getUser_id().equals(currentUser.getId()) ||
-                    currentUser.getRole().equals("ROLE_ADMIN")) {
-                ModelMapper modelMapper = new ModelMapper();
-                ContactDTO contactDTO = modelMapper.map(findContact, ContactDTO.class);
-                return new ResponseEntity<>(contactDTO, HttpStatus.OK);
-            }
-            if (authentication.isAuthenticated() && findContact.getUser_id().equals(currentUser.getId()) &&
-                    currentUser.getRole().equals("ROLE_USER")) {
-                return new ResponseEntity<>(findContact, HttpStatus.OK);
-            } else {
-                errorResponse.statusNotAuthorized("You are no authorized", "/contact");
-                return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
-            }
-        } catch (Exception e) {
-            errorResponse.statusNotFound(id, "Contact not found", "contact/");
-            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-        }
+        return contactService.getContactById(id, authentication);
     }
 
     @ApiOperation(value = "Update contact by ID", notes = "Returns this updated contact")
@@ -131,47 +54,7 @@ public class ContactController {
     @PutMapping("/{id}")
     public ResponseEntity<?> updateContact(@PathVariable("id") Long id,
                                            @RequestBody Contact contact, Authentication authentication) {
-        if (authentication == null || authentication.getName() == null) {
-            errorResponse.statusNotAuthorized("You are no authorized", "/contact");
-            return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
-        }
-        String currentName = authentication.getName();
-        User currentUser = userService.getUserByUsername(currentName);
-        try {
-            Contact oldContact = contactService.readContactById(id);
-            if (currentUser.getRole().equals("ROLE_ADMIN")) {
-                if (contact.getFirstName() != null) {
-                    oldContact.setFirstName(contact.getFirstName());
-                }
-                if (contact.getLastName() != null) {
-                    oldContact.setLastName(contact.getLastName());
-                }
-                if (contact.getPhoneNumber() != null) {
-                    oldContact.setPhoneNumber(contact.getPhoneNumber());
-                }
-                contactService.updateContact(oldContact);
-                return new ResponseEntity<>(oldContact, HttpStatus.OK);
-            }
-            if (currentUser.getId().equals(oldContact.getUser_id()) && currentUser.getRole().equals("ROLE_USER")) {
-                if (contact.getFirstName() != null) {
-                    oldContact.setFirstName(contact.getFirstName());
-                }
-                if (contact.getLastName() != null) {
-                    oldContact.setLastName(contact.getLastName());
-                }
-                if (contact.getPhoneNumber() != null) {
-                    oldContact.setPhoneNumber(contact.getPhoneNumber());
-                }
-                contactService.updateContact(oldContact);
-                return new ResponseEntity<>(oldContact, HttpStatus.OK);
-            } else {
-                errorResponse.statusNotAuthorized("You are no authorized", "/contact");
-                return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
-            }
-        } catch (Exception e) {
-            errorResponse.statusNotFound(id, "Contact not found", "contact/");
-            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-        }
+        return contactService.updateContact(id, contact, authentication);
     }
 
     @ApiOperation(value = "Delete contact by ID", notes = "Returns response with Http status")
@@ -179,32 +62,6 @@ public class ContactController {
     @DeleteMapping("/{id}")
     public ResponseEntity<ErrorResponse> deleteById(@PathVariable("id") Long id,
                                                     Authentication authentication) {
-        if (authentication == null || authentication.getName() == null) {
-            errorResponse.statusNotAuthorized("You are no authorized", "/contact");
-            return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
-        }
-        String currentName = authentication.getName();
-        User currentUser = userService.getUserByUsername(currentName);
-        try {
-            Contact findContact = contactService.readContactById(id);
-            if (currentUser.getRole().equals("ROLE_ADMIN")) {
-                contactService.deleteById(id);
-                errorResponse.statusOk(id, "Contact deleted", "contact/");
-
-                return new ResponseEntity<>(errorResponse, HttpStatus.OK);
-            }
-            if (currentUser.getId().equals(findContact.getUser_id()) && currentUser.getRole().equals("ROLE_USER")) {
-                contactService.deleteById(id);
-                errorResponse.statusOk(id, "Contact deleted", "contact/");
-
-                return new ResponseEntity<>(errorResponse, HttpStatus.OK);
-            } else {
-                errorResponse.statusNotAuthorized("You are no authorized", "/contact");
-                return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
-            }
-        } catch (Exception e) {
-            errorResponse.statusNotFound(id, "Contact not found", "contact/");
-            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-        }
+        return contactService.deleteById(id, authentication);
     }
 }
--- src/main/java/org/homeproject/ContactAPI/service/ContactService.java
+++ src/main/java/org/homeproject/ContactAPI/service/ContactService.java
@@ -1,8 +1,16 @@
 package org.homeproject.ContactAPI.service;
 
 import lombok.Data;
+import org.homeproject.ContactAPI.dto.ContactDTO;
+import org.homeproject.ContactAPI.entity.User;
+import org.homeproject.ContactAPI.error.ErrorResponse;
+import org.homeproject.ContactAPI.error.InvalidPhoneNumberException;
 import org.homeproject.ContactAPI.repository.ContactRepository;
 import org.homeproject.ContactAPI.entity.Contact;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,25 +19,165 @@ import java.util.stream.Collectors;
 @Data
 public class ContactService {
     private ContactRepository contactRepository;
+    private final ErrorResponse errorResponse;
+    private final UserService userService;
 
-    public ContactService(ContactRepository contactRepository) {
+    public ContactService(ContactRepository contactRepository, UserService userService) {
         this.contactRepository = contactRepository;
+        this.userService = userService;
+        this.errorResponse = new ErrorResponse();
     }
 
-    public Contact createContact(Contact contact) {
-        return contactRepository.save(contact);
+    public ResponseEntity<?> createContact(Contact contact, Authentication authentication) {
+        if (authentication == null || authentication.getName() == null) {
+            errorResponse.statusNotAuthorized("You are no authorized", "/contact");
+            return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+        }
+        Contact newContact = new Contact();
+        try {
+            String currentUserName = authentication.getName();
+            User currentUser = userService.getUserByUsername(currentUserName);
+            if (authentication.isAuthenticated() && currentUser.getRole().equals("ROLE_USER")) {
+                newContact =
+                        new Contact(currentUser, contact.getFirstName(), contact.getLastName(), contact.getPhoneNumber());
+                return new ResponseEntity<>(contactRepository.save(newContact), HttpStatus.OK);
+            } else {
+                errorResponse.statusNotAuthorized("You are no authorized", "/contact");
+                return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+            }
+
+        } catch (InvalidPhoneNumberException e) {
+            errorResponse.statusNotValid("Not valid phone number", "/contact");
+            return new ResponseEntity<>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
+        } catch (Exception e) {
+            return new ResponseEntity<>(contactRepository.save(newContact), HttpStatus.OK);
+        }
     }
 
-    public List<Contact> readAllContacts() {
-        return contactRepository.findAll();
+    public ResponseEntity<?> readAllContacts(Authentication authentication) {
+        if (authentication == null || authentication.getName() == null) {
+            errorResponse.statusNotAuthorized("You are no authorized", "/contact");
+            return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+        }
+        String currentUsername = authentication.getName();
+        User currentUser = userService.getUserByUsername(currentUsername);
+        if (authentication.isAuthenticated() && currentUser.getRole().equals("ROLE_ADMIN")) {
+            ModelMapper modelMapper = new ModelMapper();
+            List<Contact> allContacts = contactRepository.findAll();
+            List<ContactDTO> allContactsDTO = allContacts.stream()
+                    .map(contact -> modelMapper.map(contact, ContactDTO.class))
+                    .collect(Collectors.toList());
+            return new ResponseEntity<>(allContactsDTO, HttpStatus.OK);
+        } else {
+            List<Contact> userContacts = getContactListByUserId(currentUser.getId());
+            ModelMapper modelMapper = new ModelMapper();
+            List<ContactDTO> contactDTOList = userContacts.stream()
+                    .map(contact -> modelMapper.map(contact, ContactDTO.class))
+                    .collect(Collectors.toList());
+            return new ResponseEntity<>(contactDTOList, HttpStatus.OK);
+        }
     }
 
-    public Contact updateContact(Contact contact) {
-        return contactRepository.save(contact);
+    public ResponseEntity<?> getContactById(Long id, Authentication authentication) {
+        if (authentication == null || authentication.getName() == null) {
+            errorResponse.statusNotAuthorized("You are no authorized", "/contact");
+            return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+        }
+        String currentName = authentication.getName();
+        User currentUser = userService.getUserByUsername(currentName);
+        try {
+            Contact findContact = readContactById(id);
+            if (authentication.isAuthenticated() && findContact.getUser_id().equals(currentUser.getId()) ||
+                    currentUser.getRole().equals("ROLE_ADMIN")) {
+                ModelMapper modelMapper = new ModelMapper();
+                ContactDTO contactDTO = modelMapper.map(findContact, ContactDTO.class);
+                return new ResponseEntity<>(contactDTO, HttpStatus.OK);
+            }
+            if (authentication.isAuthenticated() && findContact.getUser_id().equals(currentUser.getId()) &&
+                    currentUser.getRole().equals("ROLE_USER")) {
+                return new ResponseEntity<>(findContact, HttpStatus.OK);
+            } else {
+                errorResponse.statusNotAuthorized("You are no authorized", "/contact");
+                return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+            }
+        } catch (Exception e) {
+            errorResponse.statusNotFound(id, "Contact not found", "contact/");
+            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        }
     }
 
-    public void deleteById(Long id) {
-        contactRepository.deleteById(id);
+    public ResponseEntity<?> updateContact(Long id, Contact contact, Authentication authentication) {
+        if (authentication == null || authentication.getName() == null) {
+            errorResponse.statusNotAuthorized("You are no authorized", "/contact");
+            return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+        }
+        String currentName = authentication.getName();
+        User currentUser = userService.getUserByUsername(currentName);
+        try {
+            Contact oldContact = readContactById(id);
+            if (currentUser.getRole().equals("ROLE_ADMIN")) {
+                if (contact.getFirstName() != null) {
+                    oldContact.setFirstName(contact.getFirstName());
+                }
+                if (contact.getLastName() != null) {
+                    oldContact.setLastName(contact.getLastName());
+                }
+                if (contact.getPhoneNumber() != null) {
+                    oldContact.setPhoneNumber(contact.getPhoneNumber());
+                }
+                contactRepository.save(oldContact);
+                return new ResponseEntity<>(oldContact, HttpStatus.OK);
+            }
+            if (currentUser.getId().equals(oldContact.getUser_id()) && currentUser.getRole().equals("ROLE_USER")) {
+                if (contact.getFirstName() != null) {
+                    oldContact.setFirstName(contact.getFirstName());
+                }
+                if (contact.getLastName() != null) {
+                    oldContact.setLastName(contact.getLastName());
+                }
+                if (contact.getPhoneNumber() != null) {
+                    oldContact.setPhoneNumber(contact.getPhoneNumber());
+                }
+                contactRepository.save(oldContact);
+                return new ResponseEntity<>(oldContact, HttpStatus.OK);
+            } else {
+                errorResponse.statusNotAuthorized("You are no authorized", "/contact");
+                return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+            }
+        } catch (Exception e) {
+            errorResponse.statusNotFound(id, "Contact not found", "contact/");
+            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public ResponseEntity<ErrorResponse> deleteById(Long id, Authentication authentication) {
+        if (authentication == null || authentication.getName() == null) {
+            errorResponse.statusNotAuthorized("You are no authorized", "/contact");
+            return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+        }
+        String currentName = authentication.getName();
+        User currentUser = userService.getUserByUsername(currentName);
+        try {
+            Contact findContact = readContactById(id);
+            if (currentUser.getRole().equals("ROLE_ADMIN")) {
+                contactRepository.deleteById(id);
+                errorResponse.statusOk(id, "Contact deleted", "contact/");
+
+                return new ResponseEntity<>(errorResponse, HttpStatus.OK);
+            }
+            if (currentUser.getId().equals(findContact.getUser_id()) && currentUser.getRole().equals("ROLE_USER")) {
+                contactRepository.deleteById(id);
+                errorResponse.statusOk(id, "Contact deleted", "contact/");
+
+                return new ResponseEntity<>(errorResponse, HttpStatus.OK);
+            } else {
+                errorResponse.statusNotAuthorized("You are no authorized", "/contact");
+                return new ResponseEntity<>(errorResponse, HttpStatus.FORBIDDEN);
+            }
+        } catch (Exception e) {
+            errorResponse.statusNotFound(id, "Contact not found", "contact/");
+            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        }
     }
 
     public Contact readContactById(Long id) {
